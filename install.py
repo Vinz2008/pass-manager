831142e8f52f7907f48be21da16a97fb0f0bd194
Vinz2008 <clubfunreading@gmail.com>
2021-09-12 17:13:46 +0200

    remove hash

--- install.py
+++ install.py
@@ -2,7 +2,7 @@ import sqlite3
 conn = sqlite3.connect('passwordmanager.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE PASSWORD
-             ([name] text,[url] text, [hash] text)''')
+             ([name] text,[url] text, [password] text)''')
 c.execute('''CREATE TABLE NUMBERPASSWORD
              ([numberpassword] integer)''')
 
--- password-manager.py
+++ password-manager.py
@@ -1,6 +1,4 @@
-import hashlib #import library for the hash
 import sqlite3 #import library for database
-import desktop_file
 conn = sqlite3.connect('passwordmanager.db') #connect to database
 c = conn.cursor()
 
@@ -9,12 +7,9 @@ number_password = 0
 def add_password():		
 	name = input("Give a name to this password : ")
 	url = input("What is the url for the password : ")
-	temp_password =  input("What is  the password : ")
-	hash_object = hashlib.sha256(temp_password.encode())
-	hex_dig = hash_object.hexdigest()
-	password_hash = hex_dig
-	c.execute('''INSERT INTO PASSWORD(name,url,hash) 
-	             VALUES (?,?,?)''',(name,url,password_hash))
+	password =  input("What is  the password : ")
+	c.execute('''INSERT INTO PASSWORD(name,url,password) 
+	             VALUES (?,?,?)''',(name,url,password))
 	conn.commit()
 def list_passwords():
 	for row in c.execute('SELECT * FROM PASSWORD ORDER BY name'):
