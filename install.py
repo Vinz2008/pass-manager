c3852be9009a542b4584714d6dd69dbf59deb9d1
Vinz2008 <clubfunreading@gmail.com>
2021-09-25 22:05:46 +0200

    added id

--- install.py
+++ install.py
@@ -4,7 +4,7 @@ import time
 conn = sqlite3.connect('passwordmanager.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE PASSWORD
-             ([name] text,[url] text, [password] text)''')
+             ([id] text,[name] text,[url] text, [password] text)''')
 c.execute('''CREATE TABLE NUMBERPASSWORD
              ([numberpassword] integer)''')
 
