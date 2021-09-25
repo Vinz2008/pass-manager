151474305ddc39563419378e7caf507876847f30
Vinz2008 <clubfunreading@@gmail.com>
2021-09-25 16:53:02 +0200

    added progress bar to install.py

--- install.py
+++ install.py
@@ -1,4 +1,6 @@
 import sqlite3
+from tqdm import tqdm
+import time
 conn = sqlite3.connect('passwordmanager.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE PASSWORD
@@ -7,3 +9,11 @@ c.execute('''CREATE TABLE NUMBERPASSWORD
              ([numberpassword] integer)''')
 
 conn.commit()
+
+  
+for i in tqdm (range (101), 
+               desc="Loading…", 
+               ascii=False, ncols=75):
+    time.sleep(0.01)
+      
+print("Complete.")
